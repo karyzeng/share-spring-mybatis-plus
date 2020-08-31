4d80cec1149b653ea5cf1afde1df07d7f8a48031
karyzeng <354697171@qq.com>
2020-08-31 17:45:42 +0800

    添加testEvent方法

--- spring.activemq.consumer/src/main/java/com/zzp/consumer/controller/ConsumerController.java
+++ spring.activemq.consumer/src/main/java/com/zzp/consumer/controller/ConsumerController.java
@@ -1,10 +1,12 @@
 package com.zzp.consumer.controller;
 
 import com.zzp.consumer.entity.TCoupon;
+import com.zzp.consumer.events.TestEvent;
 import com.zzp.consumer.service.ITCouponService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,11 +26,21 @@ public class ConsumerController {
     @Autowired
     private ITCouponService couponService;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @RequestMapping(value = "/getMessage", method = RequestMethod.GET)
     @ResponseBody
     public TCoupon getMessage() {
         TCoupon coupon = couponService.getById(1);
         return coupon;
+    }
+
+    @RequestMapping(value = "/testEvent", method = RequestMethod.GET)
+    @ResponseBody
+    public String testEvent() {
+        applicationContext.publishEvent(new TestEvent(applicationContext, "测试事件"));
+        return "发布事件成功";
     }
 
 }
