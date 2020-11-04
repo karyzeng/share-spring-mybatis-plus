4b007fe6a984fca194747b4165b26a7d47a03450
karyzeng <354697171@qq.com>
2020-11-04 19:25:20 +0800

    新增testTransactional方法

--- spring.activemq.provider/src/main/java/com/zzp/provider/service/impl/MessageServiceImpl.java
+++ spring.activemq.provider/src/main/java/com/zzp/provider/service/impl/MessageServiceImpl.java
@@ -1,9 +1,14 @@
 package com.zzp.provider.service.impl;
 
+import com.zzp.provider.entity.TSendMessage;
+import com.zzp.provider.entity.TSysUser;
 import com.zzp.provider.service.IMessageService;
+import com.zzp.provider.service.ITSendMessageService;
+import com.zzp.provider.service.ITSysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Description mq发送service接口实现类
@@ -16,6 +21,12 @@ public class MessageServiceImpl implements IMessageService {
     @Autowired
     private JmsMessagingTemplate jmsMessagingTemplate;
 
+    @Autowired
+    private ITSysUserService sysUserService;
+
+    @Autowired
+    private ITSendMessageService sendMessageService;
+
     @Override
     public void sendMessage(String destinationName, String msg) {
         jmsMessagingTemplate.convertAndSend(destinationName, msg);
@@ -24,5 +35,22 @@ public class MessageServiceImpl implements IMessageService {
     @Override
     public void sendMessage(String msg) {
         this.sendMessage("test_queue", msg);
+    }
+
+    @Override
+    @Transactional
+    public void testTransactional() {
+        TSysUser sysUser = new TSysUser();
+        sysUser.setUserName("阿尼君");
+        sysUser.setAge(18);
+        sysUserService.saveSysUser(sysUser);
+
+        TSendMessage sendMessage = new TSendMessage();
+        sendMessage.setContent("测试发送消息");
+        try {
+            sendMessageService.saveSendMessage(sendMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
